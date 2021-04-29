afeba8e2e94bc5dfa325e5ca964b101f48ca9d29
kbrien11 <49761400+kbrien11@users.noreply.github.com>
2021-04-29 18:52:24 -0400

    Update autocomplete.js

--- src/components/autocomplete.js
+++ src/components/autocomplete.js
@@ -1,15 +1,39 @@
+
 import algoliasearch from 'algoliasearch';
 import instantsearch from 'instantsearch.js';
+import { connectHits } from 'instantsearch.js/es/connectors';
 
 // Instant Search Widgets
-import { hits, searchBox, configure } from 'instantsearch.js/es/widgets';
+import { hits, searchBox, configure, index } from 'instantsearch.js/es/widgets';
 
 // Autocomplete Template
 import autocompleteProductTemplate from '../templates/autocomplete-product';
 
+const renderQSHits = ({ widgetParams, hits }, isFirstRender) => {
+  console.log(hits);
+  const container = document.querySelector(widgetParams.container);
+  container.innerHTML = `<ul>
+   ${hits
+     .map(
+       item => `
+        
+       <li>${instantsearch.highlight({
+         hit: item,
+         attribute: 'query',
+       })}</li>
+     `
+     )
+     .join('')}
+ </ul>`;
+};
+
+const QSHits = connectHits(renderQSHits);
+
 /**
  * @class Autocomplete
  * @description Instant Search class to display content in the page's autocomplete
+ *
+ *
  */
 class Autocomplete {
   /**
@@ -28,12 +52,12 @@ class Autocomplete {
    */
   _registerClient() {
     this._searchClient = algoliasearch(
-      'VYLEWMPKEZ',
-      '8940a18fde155adf3f74b0912c267aa4'
+      '7J2ITFTP1T',
+      'c65064ea779bda0cdbbaa63bd9659cfe'
     );
 
     this._searchInstance = instantsearch({
-      indexName: 'ecommerce-v2',
+      indexName: 'products',
       searchClient: this._searchClient,
     });
   }
@@ -44,17 +68,38 @@ class Autocomplete {
    * @return {void}
    */
   _registerWidgets() {
+    // Customize UI of the facet column
     this._searchInstance.addWidgets([
       configure({
-        hitsPerPage: 3,
+        hitsPerPage: 5,
       }),
       searchBox({
         container: '#searchbox',
+        placeholder: 'Search for products',
+        showReset: true,
+        showSubmit: true,
+        showLoadingIndicator: true,
       }),
       hits({
         container: '#autocomplete-hits',
+        placeholder: 'Search for products',
         templates: { item: autocompleteProductTemplate },
       }),
+
+      index({
+        indexName: 'products',
+      }),
+
+      index({
+        indexName: 'williams_query_suggestions',
+      }).addWidgets([
+        configure({
+          hitsPerPage: 10,
+        }),
+        QSHits({
+          container: '#suggestions',
+        }),
+      ]),
     ]);
   }
 
